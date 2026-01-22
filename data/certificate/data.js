b899a859256f2423445025594c3ae29ab8d74a23
tomoya <py2qiuse@gmail.com>
2026-01-22 17:56:16 +0800

    1

---
+++ data/certificate/data.js
@@ -0,0 +1,364 @@
+[
+  {
+    category: "证书",
+    child: [
+      {
+        name: "空证书",
+        image: "certificate_blank.png",
+        tech: "制图桌",
+        recipe: "莎草纸*1+羽毛笔*1",
+        description: `
+一张等待被书写的空证书
+            `,
+      },
+      {
+        name: "读者证书",
+        image: "certificate_book.png",
+        tech: "制图桌",
+        recipe: "莎草纸*1+羽毛笔*1+恶魔燃料*1",
+        description: `
+注：装备前要使用羽毛笔签上自己的名字
+
+读一次书降5耐久，并记录下读的书，再读已经记录的书仅降1耐久
+
+大约读18本不同的书后可完全解锁获得图书管理员阅读书籍且能生效的能力
+            `,
+      },
+      {
+        name: "竞技证书",
+        image: "certificate_combat.png",
+        tech: "制图桌",
+        recipe: "莎草纸*1+羽毛笔*1+狗牙*1",
+        description: `
+注：装备前要使用羽毛笔签上自己的名字
+
+击杀头目且头目血量在10k及以上的话，首次击杀降10耐久，否则降5耐久。
+
+击杀非头目生物时血量在10k及以上的话，首次击杀降5耐久，否则降2耐久
+
+耐久降为0时解锁战斗证书，穿戴后12.5%吸收伤害（女武神25%），战斗时可缓慢的回复精神和血量，能制作女武神战斗矛和头盔
+            `,
+      },
+      {
+        name: "厨师证书",
+        image: "certificate_cook.png",
+        tech: "制图桌",
+        recipe: "莎草纸*1+羽毛笔*1+肉丸*1",
+        description: `
+注：装备前要使用羽毛笔签上自己的名字
+
+首次收获自己烹饪的料理时可降5耐久，再次收获相同料理仅降1耐久
+
+大约收获40道不同料理能解锁厨师证书，穿戴后可使用大厨锅和调味站
+            `,
+      },
+      {
+        name: "渔夫证书",
+        image: "certificate_fish.png",
+        tech: "制图桌",
+        recipe: "莎草纸*1+羽毛笔*1+淡水鱼*1",
+        description: `
+注：装备前要使用羽毛笔签上自己的名字
+
+青蛙池塘，蚊子池塘，绿洲，洞穴池塘，选3种池塘每个池塘钓10条鱼解锁
+
+穿戴后钓鱼秒上钩
+            `,
+      },
+      {
+        name: "手工证书",
+        image: "certificate_hand.png",
+        tech: "制图桌",
+        recipe: "莎草纸*1+羽毛笔*1+蜘蛛丝*1",
+        description: `
+注：装备前要使用羽毛笔签上自己的名字
+
+首次制作物品可降5耐久，再次制作已经记录的物品降1耐久
+
+穿戴后搓东西很快，可制作女工胶布
+            `,
+      },
+      {
+        name: "开荒证书",
+        image: "certificate_till.png",
+        tech: "制图桌",
+        recipe: "莎草纸*1+羽毛笔*1+锄头*1",
+        description: `
+注：装备前要使用羽毛笔签上自己的名字
+
+锄100次地后解锁
+
+穿戴后每次锄地有25%概率锄出蚯蚓
+            `,
+      },
+      {
+        name: "农业证书",
+        image: "certificate_plant.png",
+        tech: "制图桌",
+        recipe: "莎草纸*1+羽毛笔*1+开荒证书*1",
+        description: `
+注：装备前要使用羽毛笔签上自己的名字
+
+收获自己种的巨大作物时降1耐久
+
+穿戴后每次锄地有25%概率锄出蚯蚓，且能像植物人那样随手种地
+            `,
+      },
+      {
+        name: "采矿证书",
+        image: "certificate_rock.png",
+        tech: "制图桌",
+        recipe: "莎草纸*1+羽毛笔*1+金块*1",
+        description: `
+注：装备前要使用羽毛笔签上自己的名字
+
+每次使用镐子降1耐久
+
+穿戴后镐子效率增加250%
+            `,
+      },
+      {
+        name: "伐木证书",
+        image: "certificate_tree.png",
+        tech: "制图桌",
+        recipe: "莎草纸*1+羽毛笔*1+木材*1",
+        description: `
+注：装备前要使用羽毛笔签上自己的名字
+
+每次使用斧子降1耐久
+
+穿戴后斧子效率增加250%
+            `,
+      },
+      {
+        name: "魔法证书",
+        image: "certificate_magic.png",
+        description: `
+注：装备前要使用羽毛笔签上自己的名字
+
+去舞台表演，有1/8概率获得
+
+可能是老麦表演太卖力不小心遗失的
+
+穿戴后可制作魔法箱子和魔杖
+            `,
+      },
+      {
+        name: "兔王任命书",
+        image: "certificate_speed.png",
+        description: `
+注：装备前要使用羽毛笔签上自己的名字
+
+击杀机缘兔子召唤的兔子守卫有概率掉落
+
+守卫有任命书很正常吧！
+
+穿戴后可制作兔王手杖
+            `,
+      },
+      {
+        name: "航海证书",
+        image: "certificate_ocean.png",
+        description: `
+注：装备前要使用羽毛笔签上自己的名字
+
+击杀大副有概率掉落
+
+在永恒大陆大副也得持证抢劫！
+
+穿戴后划船比大头还快
+            `,
+      },
+      {
+        name: "鱼人王任命书",
+        image: "certificate_merm.png",
+        description: `
+注：装备前要使用羽毛笔签上自己的名字
+
+鱼妹将空证书给鱼人王后，鱼人王会返还一个鱼人王任命书
+
+穿戴后可喂鱼人王，喂鱼人，让鱼人跟随成为小弟
+            `,
+      },
+      {
+        name: "驯兽师证书",
+        image: "certificate_tame.png",
+        description: `
+注：装备前要使用羽毛笔签上自己的名字
+
+装备着未解锁的驯兽师证书，自己驯的牛达到100%驯服度时自动解锁
+
+穿戴后再驯牛能加速20%（女武神技能加速15%），养高鸟时，每天可抚摸一次高鸟并减少一天的成长时间
+            `,
+      },
+    ]
+  },
+  {
+    category: "作物",
+    child: [
+      {
+        name: "红薯植株",
+        image: "cer_sweetpotato.png",
+        description: `
+随机种子可长出红薯植株
+
+稀有度：普通
+
+季节：春夏秋作物
+
+属性：水x1，生长素x-2，堆肥x4，粪肥x-2
+
+产物：红薯
+            `,
+      },
+      {
+        name: "生姜植株",
+        image: "cer_ginger.png",
+        description: `
+随机种子可长出生姜植株
+
+稀有度：普通
+
+季节：春夏秋冬作物
+
+属性：水x1，生长素x-2，堆肥x1，粪肥x1
+
+产物：生姜
+            `,
+      },
+      {
+        name: "红薯",
+        image: "cer_sweetpotato.png",
+        code: "cer_sweetpotato",
+        foodtype: "VEGGIE",
+        fresh: "10天",
+        description: `
+可生吃，可烤着吃，可榨油，可入锅当配菜
+
+|饱|精|血|其它|
+|---|---|---|---|
+|25|1|3||
+            `,
+      },
+      {
+        name: "烤红薯",
+        image: "cer_sweetpotato_cooked.png",
+        code: "cer_sweetpotato_cooked",
+        foodtype: "VEGGIE",
+        fresh: "6天",
+        description: `
+|饱|精|血|其它|
+|---|---|---|---|
+|25|20|15||
+            `,
+      },
+      {
+        name: "红薯干",
+        image: "cer_sweetpotato_dried.png",
+        code: "cer_sweetpotato_dried",
+        foodtype: "VEGGIE",
+        fresh: "40天",
+        description: `
+2天晾干
+
+|饱|精|血|其它|
+|---|---|---|---|
+|25|5|8||
+            `,
+      },
+      {
+        name: "生姜",
+        image: "cer_ginger.png",
+        code: "cer_ginger",
+        foodtype: "VEGGIE",
+        fresh: "20天",
+        description: `
+2天晾干
+
+|饱|精|血|其它|
+|---|---|---|---|
+|9.375|-5|-3||
+            `,
+      },
+    ]
+  },
+  {
+    category: "料理",
+    child: [
+      {
+        name: "姜汤",
+        image: "cer_gingersoup.png",
+        code: "cer_gingersoup",
+        foodtype: "好东西",
+        fresh: "6天",
+        description: `
+生姜*2 + 洋葱*1 + 冰*1
+
+|饱|精|血|其它|
+|---|---|---|---|
+|62.5|15|15|喝下后持续一天的保暖状态|
+            `,
+      },
+      {
+        name: "汉堡",
+        image: "cer_hamburger.png",
+        code: "cer_hamburger",
+        foodtype: "好东西",
+        fresh: "10天",
+        description: `
+肉度*1 + 蕨菜*1 + 菜度*1
+
+|饱|精|血|其它|
+|---|---|---|---|
+|62.5|15|15||
+            `,
+      },
+      {
+        name: "红薯丸子",
+        image: "cer_sweetpotatoballs.png",
+        code: "cer_sweetpotatoballs",
+        foodtype: "好东西",
+        fresh: "10天",
+        description: `
+红薯*2 + 甜度*1
+
+|饱|精|血|其它|
+|---|---|---|---|
+|37.5|50|20||
+            `,
+      },
+      {
+        name: "清蒸鱼",
+        image: "cer_steamedfish.png",
+        code: "cer_steamedfish",
+        foodtype: "好东西",
+        fresh: "8天",
+        description: `
+鱼度*1 + 生姜*1
+
+|饱|精|血|其它|
+|---|---|---|---|
+|25|33|40||
+            `,
+      },
+      {
+        name: "姜糖",
+        image: "cer_gingercandy.png",
+        code: "cer_gingercandy",
+        foodtype: "好东西",
+        fresh: "40天",
+        description: `
+生姜*2 + 甜度*1
+
+|饱|精|血|其它|
+|---|---|---|---|
+|25|15|8||
+            `,
+      },
+    ]
+  },
+  {
+    category: "调味",
+    child: []
+  }
+];
